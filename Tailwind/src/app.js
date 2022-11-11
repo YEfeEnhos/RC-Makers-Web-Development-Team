b2c974cd96094ce1d59dddac161c7c0a910720b6
YEfeEnhos <enhyig.25@robcol.k12.tr>
2022-11-11 08:24:01 +0300

    5. hafta ödevleri cevap anahtarı

---
+++ Tailwind/src/app.js
@@ -0,0 +1,8 @@
+// grab everything we need
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+// add event listeners
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
---
+++ Tailwind/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
